d7dffe9155d5b857955810d678a58ba592b8721a
gatimeher <gatimeher@yahoo.com>
2018-04-09 12:29:59 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/whitehat/travis/plugin/TravisPluginApplication.java
@@ -0,0 +1,12 @@
+package com.whitehat.travis.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisPluginApplication.class, args);
+	}
+}
